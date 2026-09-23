37112f7942600c6449eaab29262acc9bcb2f05d0
Saqr Anes <saqranes770@gmail.com>
2026-09-23 22:18:12 +0300

    Add Body class and main script to display user information

---
+++ Body.py
@@ -0,0 +1,12 @@
+class body:
+    def __init__(self):
+        self.name = "Body"
+        self.age = 30
+        self.gender = "Male"
+    def print_info(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Gender:",self.gender)
+
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from Body import body
+print("Welcome ..........")
+person= body()
+person.print_info()
